555c499bdb64d0c0fd4fc56da50ecbf70c10dc3e
Woody <devkritt@gmail.com>
2025-01-09 14:44:19 +0700

    Create Animator for Both Side of Player

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -1,10 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class UIController : MonoBehaviour
 {
+    public Animator player1_anim;
+    public string player1_name;
+
+    public Animator player2_anim;
+    public string player2_name;
+
+    public AnimatorController blueOrg;
+    public AnimatorController greenOrg;
+    public AnimatorController lightGreenOrg;
+    public AnimatorController pinkOrg;
+    public AnimatorController redOrg;
+    public AnimatorController whiteOrg;
+
     [SerializeField] private Transform playerDeadHolder;
     [SerializeField] private Transform enemyDeadHolder;
     [SerializeField] private GameObject m_playerPiece;
